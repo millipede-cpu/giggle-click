0b8ee4ae3c9129852bac4464f40f104172f92362
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-09-13 17:15:22 +0100

    adding the functional component and props

--- GiggleClick/src/AppliedTheFavouriteJoke.tsx
+++ GiggleClick/src/AppliedTheFavouriteJoke.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export const AppliedTheFavouriteJoke = () => {
+// props being added above the component.
+// once the props are declared then the AppliedTheFavouriteJoke will be a functional component. This then passes the props as generic.
+
+// the NextBigJoke will have text as string and complete property as boolean.
+interface NextBigJoke {
+  text: string;
+  complete: boolean;
+}
+
+interface props {
+  newJokeToLaugh: NextBigJoke;
+}
+
+export const AppliedTheFavouriteJoke: React.FC<props> = (props) => {
   return <li>HAHAHA THATS HILARIOUS!!!</li>;
 };

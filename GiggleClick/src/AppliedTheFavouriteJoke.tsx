d9b6a37420e38162fa3b733ecd3e9080d31cdb4c
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-09-13 17:44:02 +0100

    removed types as they are declared within type declaration file

--- GiggleClick/src/AppliedTheFavouriteJoke.tsx
+++ GiggleClick/src/AppliedTheFavouriteJoke.tsx
@@ -4,10 +4,10 @@ import React from "react";
 // once the props are declared then the AppliedTheFavouriteJoke will be a functional component. This then passes the props as generic.
 
 // the NextBigJoke will have text as string and complete property as boolean.
-interface NextBigJoke {
-  text: string;
-  complete: boolean;
-}
+// interface NextBigJoke {
+//   text: string;
+//   complete: boolean;
+// }
 
 interface props {
   newJokeToLaugh: NextBigJoke;

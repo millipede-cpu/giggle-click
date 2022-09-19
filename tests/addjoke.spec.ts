8076ac64e898a2ae082382f7e6226ba59e4fa57a
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-09-20 00:08:20 +0100

    TDD method to test a joke is added and submitted to the page
    co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- tests/addjoke.spec.ts
+++ tests/addjoke.spec.ts
@@ -2,9 +2,13 @@ import { test, expect } from "@playwright/test";
 
 test("enter a joke and see it rendered on a page", async ({ page }) => {
   await page.goto("http://localhost:5173");
-  await page.locator('label="add-joke"]').isVisible();
   await page
-    .locator("text=add-joke")
+    .locator('input[id="joke"]')
     .fill("Do tomatoes and potatoes have anything in common?Toes.");
-  await page.locator("button").click();
+  await page.locator('button[class="submit-joke"]').click();
+  await page
+    .locator(
+      'span:has-text("Do tomatoes and potatoes have anything in common?Toes.")'
+    )
+    .isVisible();
 });

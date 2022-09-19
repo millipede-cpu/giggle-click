34be4108f6b6ad94cdf0021a717a59bcbb7273d0
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-09-19 23:13:21 +0100

    check is label is visible and test type input area to fill joke
    co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- tests/addjoke.spec.ts
+++ tests/addjoke.spec.ts
@@ -1,1 +1,9 @@
 import { test, expect } from "@playwright/test";
+
+test("enter a joke and see it rendered on a page", async ({ page }) => {
+  await page.goto("http://localhost:5173");
+  await page.locator('label="add-joke"]').isVisible();
+  await page
+    .locator("text=add-joke")
+    .fill("Do tomatoes and potatoes have anything in common?Toes.");
+});

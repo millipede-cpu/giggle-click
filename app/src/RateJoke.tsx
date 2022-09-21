688d36b5b00628ce4fa84d4ef01676592a8fe496
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-09-21 20:02:02 +0100

    conditional render of happy and sad emoji when onclick function fires
    co-authored-by: Milly <millipede-cpu@users.noreply.github.com>
    co-authored-by: Peter <PJSalter@users.noreply.github.com>

--- app/src/RateJoke.tsx
+++ app/src/RateJoke.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 export default function RateJoke() {
+  const [happy, setHappy] = useState(false);
+  const [sad, setSad] = useState(false);
+
   return (
     <>
-      <span className="happy">&#128514;</span>
-      <span className="sad">&#128529;</span>
+      <button onClick={() => setHappy(!happy)}>Happy</button>
+      {happy ? <span className="happy">&#128514;</span> : null}
+
+      <button onClick={() => setSad(!sad)}>Sad</button>
+      {sad ? <span className="sad">&#128529;</span> : null}
     </>
   );
 }

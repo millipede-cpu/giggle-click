ada174f7a932a52afbe91865e3e97c44680ecd5a
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-09-21 19:11:25 +0100

    component to rate on if joke is happy or sad
    co-authored-by: Milly <millipede-cpu@users.noreply.github.com>
    co-authored-by: Peter <PJSalter@users.noreply.github.com>

---
+++ app/src/RateJoke.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function RateJoke() {
+  return (
+    <>
+      <span className="happy">Happy</span>
+      <span className="sad">Sad</span>
+    </>
+  );
+}

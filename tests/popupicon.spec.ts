b604973a3340531b9fbee0b01547e060771edda3
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-09-21 19:05:16 +0100

    locating happy and sad
    co-authored-by: Milly <millipede-cpu@users.noreply.github.com>
    co-authored-by: Peter <PJSalter@users.noreply.github.com>

--- tests/popupicon.spec.ts
+++ tests/popupicon.spec.ts
@@ -1,1 +1,7 @@
 import { test, expect } from "@playwright/test";
+
+test("allocating icons for happy and sad joke response", async ({ page }) => {
+  await page.goto("http://localhost:5173");
+  await page.locator('span[class="happy"]');
+  await page.locator('span[class="sad"]');
+});

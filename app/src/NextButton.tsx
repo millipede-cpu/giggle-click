277b0bbda12d465e345e25daa0b17bebb0340974
Milly-Palmer <milly@actioncytech.com>
2022-09-24 21:50:44 +0100

    Convert NextButton to JSX.Element function
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- app/src/NextButton.tsx
+++ app/src/NextButton.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
-export default function NextButton() {
+export default function NextButton(): JSX.Element {
   const navigate = useNavigate();
+
   return <button onClick={() => navigate(1)}>Click for giggles 🤡🤡🤡</button>;
 }

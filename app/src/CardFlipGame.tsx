189361ce35a1c7eb45a56e1b7eb99edefebbb8d5
Milly-Palmer <milly@actioncytech.com>
2022-09-24 20:55:52 +0100

    Create CardFlipGame component
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

---
+++ app/src/CardFlipGame.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function CardFlipGame() {
+  return <Title>Card Flip Game</Title>;
+}
+
+const Title = styled.h1`
+  font-size: 3rem;
+  color: #0eb0f3;
+`;

caaa1cccb05947be713c7686ac8d3ed20342f7b1
Milly-Palmer <milly@actioncytech.com>
2022-09-24 20:06:51 +0100

    Create NextButton component
    co-authored-by: Peter <PJSalter@users.noreply.github.com>

---
+++ app/src/NextButton.tsx
@@ -0,0 +1,7 @@
+export default function NextButton() {
+  return (
+    <button onClick={() => console.log("hahahah")}>
+      Hi I'm a button! Click me for more giggles! 🤡🤡🤡 Hehe
+    </button>
+  );
+}

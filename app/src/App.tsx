eb0281c324b7926fe3680c10a331a31f3b79acc8
Milly-Palmer <milly@actioncytech.com>
2022-09-24 21:48:35 +0100

    Relocate page styling and functionality to Home comp onent, App.tsx is only for page routes
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,78 +1,36 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AppliedTheFavouriteJoke } from "./AppliedTheFavouriteJoke";
-import AddJoke from "./AddJoke";
-import styled from "styled-components";
-import NextButton from "./NextButton";
 import JokeRandomiser from "./JokeRandomiser";
 import RateJoke from "./RateJoke";
 import CardFlipGame from "./CardFlipGame";
+import Home from "./Home";
 
-const newJokeToLaugh: NextBigJoke[] = [
-  {
-    text: "What do you call a yam with a broom ? A sweep potato.",
-    complete: false,
-  },
-  {
-    text: "Why did the ice cream van break down? Because there was a rocky road!",
-    complete: true,
-  },
-];
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <AppStyle>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">Home</Route>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              onClick={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+          }
+        ></Route>
 
-          <Route path="/joke-randomiser" element={<JokeRandomiser />}>
-            Joke Randomiser
-          </Route>
+        <Route path="/joke-randomiser" element={<JokeRandomiser />}>
+          Joke Randomiser
+        </Route>
 
-          <Route path="/card-flip-game" element={<CardFlipGame />}>
-            Card Flip Game
-          </Route>
+        <Route path="/card-flip-game" element={<CardFlipGame />}>
+          Card Flip Game
+        </Route>
 
-          <Route path="/rate-joke" element={<RateJoke />}>
-            Rate Joke
-          </Route>
-        </Routes>
-        <div></div>
-        <StyledTitle>Giggle Click</StyledTitle>
-        <ul>
-          <AppliedTheFavouriteJoke newJokeToLaugh={newJokeToLaugh[0]} />
-          <AppliedTheFavouriteJoke newJokeToLaugh={newJokeToLaugh[1]} />
-        </ul>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <AddJoke />
-          <NextButton />
-        </div>
-      </BrowserRouter>
-    </AppStyle>
+        <Route path="/rate-joke" element={<RateJoke />}>
+          Rate Joke
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
-
-const AppStyle = styled.div`
-  background-color: #add8e6;
-  display: flex;
-  // flex-wrap: nowrap;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 100%;
-  height: 100vh;
-`;
-
-const StyledTitle = styled.h1`
-  color: #fd7094;
-`;

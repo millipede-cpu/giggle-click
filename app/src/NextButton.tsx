05193ba024a532a9aee6923d2e091adda51ec477
Milly-Palmer <milly@actioncytech.com>
2022-09-24 23:04:43 +0100

    Add className to button element for tests to pick up this attribute
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- app/src/NextButton.tsx
+++ app/src/NextButton.tsx
@@ -3,5 +3,9 @@ import { useNavigate } from "react-router-dom";
 export default function NextButton(): JSX.Element {
   const navigate = useNavigate();
 
-  return <button onClick={() => navigate(1)}>Click for giggles 🤡🤡🤡</button>;
+  return (
+    <button className="next-button" onClick={() => navigate(1)}>
+      Click for giggles 🤡🤡🤡
+    </button>
+  );
 }

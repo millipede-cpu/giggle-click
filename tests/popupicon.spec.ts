cd2e0d7be33e01cb74dbdd95b47f40df9bd2414b
Milly-Palmer <milly@actioncytech.com>
2022-09-24 22:24:36 +0100

    fixed test issue
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- tests/popupicon.spec.ts
+++ tests/popupicon.spec.ts
@@ -7,7 +7,7 @@ test("allocating icons for happy and sad joke response", async ({ page }) => {
   // });
   // const context = await browser.newContext();
   // const page = await context.newPage();
-  await page.goto("http://localhost:5173");
+  await page.goto("http://localhost:5173/rate-joke");
   await page.locator('button[class="happy-click"]').click();
   await page.locator('span[class="happy"]').isVisible();
   await page.locator('button[class="sad-click"]').click();

9d136b6fc3c0cbceb1297a211467a285f9cff7a0
Milly-Palmer <milly@actioncytech.com>
2022-09-28 23:43:31 +0100

    Update form with mouse event handlers
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- app/src/AddJoke.tsx
+++ app/src/AddJoke.tsx
@@ -1,27 +1,40 @@
 import React, { useState } from "react";
 
-export default function AddJoke() {
-  const [joke, setAddJoke] = useState("");
-  const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+export default function AddJoke({ addJoke }: any) {
+  const [joke, setAddJoke] = useState({ addJoke: "", punchline: "" });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAddJoke({ ...joke, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // alert(joke);
+    addJoke(joke);
+    setAddJoke({ addJoke: "", punchline: "" });
   };
 
   return (
-    <form onSubmit={submitForm}>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="joke"></label>
       <input
         placeholder="Add a new joke"
-        value={joke}
-        onChange={(e) => setAddJoke(e.target.value)}
+        value={joke.addJoke}
+        onChange={handleChange}
         type="text"
         id="joke"
         name="joke"
-      ></input>
-      <button type="submit" className="submit-joke">
+      />
+      <input
+        placeholder="Add the punchline"
+        value={joke.punchline}
+        onChange={handleChange}
+        type="text"
+        id="punchline"
+        name="punchline"
+      />
+      <button onClick={() => joke} className="submit-joke">
         Submit Joke
       </button>
-      <span>{joke}</span>
     </form>
   );
 }

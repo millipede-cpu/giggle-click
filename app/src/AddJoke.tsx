3c8adf3f5f1946bd9e8d28de7dbce37e8b266853
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-10-04 23:57:46 +0100

    refactored component
    co-authored-by: Milly <millipede-cpu@users.noreply.github.com>
    co-authored-by: Peter <PJSalter@users.noreply.github.com>

--- app/src/AddJoke.tsx
+++ app/src/AddJoke.tsx
@@ -1,40 +1,71 @@
-import React, { useState } from "react";
+// export default function AddJoke({ addJoke }: any) {
 
-export default function AddJoke({ addJoke }: any) {
-  const [joke, setAddJoke] = useState({ addJoke: "", punchline: "" });
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAddJoke({ ...joke, [event.target.name]: event.target.value });
+interface Props {
+  addJoke: AddJokes;
+}
+
+const AddJoke: React.FC<Props> = ({ addJoke }) => {
+  const [text, setText] = useState<string>("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setText(text);
+    setText(event.target.value);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: FormEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    addJoke(joke);
-    setAddJoke({ addJoke: "", punchline: "" });
+    addJoke(text);
+    setText("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="joke"></label>
-      <input
-        placeholder="Add a new joke"
-        value={joke.addJoke}
-        onChange={handleChange}
-        type="text"
-        id="joke"
-        name="joke"
-      />
-      <input
-        placeholder="Add the punchline"
-        value={joke.punchline}
-        onChange={handleChange}
-        type="text"
-        id="punchline"
-        name="punchline"
-      />
-      <button onClick={() => joke} className="submit-joke">
-        Submit Joke
+    <form>
+      <input type="text" value={text} onChange={handleChange} />
+      <button type="submit" onClick={handleSubmit}>
+        Add Joke
       </button>
     </form>
   );
-}
+};
+
+export default AddJoke;
+
+// const [joke, setAddJoke] = useState({ addJoke: "", punchline: "" });
+
+// const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//   setAddJoke({ ...joke, [event.target.name]: event.target.value });
+// };
+
+// const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//   event.preventDefault();
+//   addJoke(joke);
+//   setAddJoke({ addJoke: "", punchline: "" });
+// };
+
+// return (
+//   <form onSubmit={handleSubmit}>
+//     <label htmlFor="joke"></label>
+//     <input
+//       placeholder="Add a new joke"
+//       value={joke.addJoke}
+//       onChange={handleChange}
+//       type="text"
+//       id="joke"
+//       name="joke"
+//     />
+//     <input
+//       placeholder="Add the punchline"
+//       value={joke.punchline}
+//       onChange={handleChange}
+//       type="text"
+//       id="punchline"
+//       name="punchline"
+//     />
+//     <button onClick={() => joke} className="submit-joke">
+//       Submit Joke
+//     </button>
+//   </form>
+// );
+// }

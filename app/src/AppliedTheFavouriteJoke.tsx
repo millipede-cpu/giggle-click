7fc9a189881bfea3d9f7df433e402f2998d5a32b
Milly-Palmer <milly@actioncytech.com>
2022-10-23 22:35:27 +0100

    Refactor AppliedTheFavouriteJoke to render joke and pun on Home Paeg
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- app/src/AppliedTheFavouriteJoke.tsx
+++ app/src/AppliedTheFavouriteJoke.tsx
@@ -1,51 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
+import { IJokes } from "./interfaces";
 
-// props being added above the component.
-// once the props are declared then the AppliedTheFavouriteJoke will be a functional component. This then passes the props as generic.
-
-// the NextBigJoke will have text as string and complete property as boolean.
-// interface NextBigJoke {
-//   text: string;
-//   complete: boolean;
-// }
-
-interface ApplyJokeProps {
-  newJokeToLaugh: {
-    text: string;
-    complete: boolean;
-  };
-  toggleJoke: ToggleJoke;
+interface Props {
+  joke: IJokes;
 }
-type Joke = {
-  text: string;
-  complete: boolean;
-};
 
-type ToggleJoke = (selectedJoke: Joke) => void;
-
-export const AppliedTheFavouriteJoke: React.FC<ApplyJokeProps> = ({
-  newJokeToLaugh,
-  toggleJoke,
-}) => {
+export const AppliedTheFavouriteJoke = ({ joke }: Props) => {
   // placing a checkbox in each list item
   // the checkbox will be checked once complete type is true.
   // This will have a label with the text of the joke.
   return (
-    <li>
-      <label
-        htmlFor="add-joke"
-        style={{
-          textDecoration: newJokeToLaugh.complete ? "line-through" : undefined,
-        }}
-      >
-        Add Joke
-      </label>
-      <input
-        type="checkbox"
-        onChange={() => toggleJoke(newJokeToLaugh)}
-        defaultChecked={newJokeToLaugh.complete}
-      />
-      {/* HAHAHA THATS HILARIOUS!!! */}
-    </li>
+    <>
+      <div>
+        {joke.jokeName} {joke.punName}
+      </div>
+    </>
   );
 };

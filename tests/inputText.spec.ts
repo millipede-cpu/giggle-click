1e6c387f647fa646cb124dd5fa8c440449424a5a
Milly-Palmer <milly@actioncytech.com>
2022-10-23 23:22:35 +0100

    Refactor inputText test
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- tests/inputText.spec.ts
+++ tests/inputText.spec.ts
@@ -1,21 +1,28 @@
-import { chromium, test, expect } from "@playwright/test";
+import { chromium, test } from "@playwright/test";
 
-test("Typing a joke and punchline should only appear on the page after clicking the Submit Joke button", async () => {
+test("Typing a joke and pun should only appear on the page after clicking the Submit Joke button", async () => {
   const browser = await chromium.launch({
     headless: false,
     slowMo: 3000,
   });
   const context = await browser.newContext();
   const page = await context.newPage();
-  // Typing text shouldn't appear on the home page
-  // until Submit Joke button is clicked
   await page
-    .locator('input[id="joke"]')
-    .fill(
-      "Why do scientists not trust atoms? Because they make up everything."
-    );
-  await page.locator('button[class="submit-joke"]').click();
+    .locator("#joke", {
+      hasText: "Why do scientists not trust atoms?",
+    })
+    .locator(".submit-joke")
+    .click({ force: true });
   await page.locator(
-    'p:has-text("Why do scientists not trust atoms? Because they make up everything.")'
+    '.showJokePun:has-text("Why do scientists not trust atoms?")'
+  );
+  await page
+    .locator("#pun", {
+      hasText: "Because they make up everything.",
+    })
+    .locator(".submit-pun")
+    .click({ force: true });
+  await page.locator(
+    '.showJokePun:has-text("Because they make up everything.")'
   );
 });

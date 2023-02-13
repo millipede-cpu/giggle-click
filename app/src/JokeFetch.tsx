de71c34191252baf2874dc91d4e0639ef3f6abd1
Milly-Palmer <milly@actioncytech.com>
2023-02-13 11:35:24 +0000

    Fetch joke as json from api

--- app/src/JokeFetch.tsx
+++ app/src/JokeFetch.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 interface JokeFetchProps {
-  jokeRandom: string;
+  jokeHolder: string;
+  jokeButton: string;
 }
 
-const JokeFetch: React.FC<JokeFetchProps> = (jokeRandom) => {
-  return <div>Random Joke to be fetched from https://icanhazdadjoke.com/</div>;
+async function fetchJoke() {
+  const response = await fetch("https://icanhazdadjoke.com", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  return response.json();
+}
+
+async function handleClick() {
+  const { joke } = await fetchJoke();
+  console.log(joke);
+}
+const JokeFetch: React.FC<JokeFetchProps> = (jokeHolder, jokeButton) => {
+  return (
+    <div>
+      Random Joke to be fetched from https://icanhazdadjoke.com/
+      <p>Dad Jokes </p>
+      <button onClick={handleClick}>Get a Joke</button>
+    </div>
+  );
 };
 
 export default JokeFetch;

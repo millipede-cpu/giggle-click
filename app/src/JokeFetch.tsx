fb651db7b60225f02449f715d8d708c00c0fd9f5
Milly-Palmer <milly@actioncytech.com>
2023-02-19 23:30:27 +0000

    JokeFetch route for random DadJokes, toggle button and random joke review button

--- app/src/JokeFetch.tsx
+++ app/src/JokeFetch.tsx
@@ -1,31 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import BackButton from "./BackButton";
+import NextButton from "./NextButton";
 
-interface JokeFetchProps {
-  jokeHolder: string;
-  jokeButton: string;
-}
+const buttonText = [
+  "Ugh.",
+  "🤦🏻‍♂️",
+  "omg dad.",
+  "you are the worst",
+  "seriously",
+  "stop it.",
+  "please stop",
+  "that was the worst one",
+];
 
-async function fetchJoke() {
-  const response = await fetch("https://icanhazdadjoke.com", {
-    headers: {
-      Accept: "application/json",
-    },
-  });
-  return response.json();
-}
+const JokeFetch = () => {
+  /* Render json from fetchJoke()
+     when handleClick() event fires */
+  const [joke, setJoke] = useState("");
+  /* Toggle button with boolean values  */
+  const [toggle, setToggle] = useState(false);
+  /* Randomise array string value "Joke Review"
+     onClick button event fires */
+  const [index, setIndex] = useState(-1);
 
-async function handleClick() {
-  const { joke } = await fetchJoke();
-  console.log(joke);
-}
-const JokeFetch: React.FC<JokeFetchProps> = (jokeHolder, jokeButton) => {
+  const startName = [""];
+  const randomJokeReview = index !== -1 ? buttonText[index] : startName[0];
+
+  async function fetchJoke() {
+    const response = await fetch("https://icanhazdadjoke.com", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const data = response.json();
+    return data;
+  }
+
+  async function handleClick() {
+    const { joke } = await fetchJoke();
+    setJoke(joke);
+  }
+
   return (
-    <div>
-      Random Joke to be fetched from https://icanhazdadjoke.com/
-      <p>Dad Jokes </p>
-      <button onClick={handleClick}>Get a Joke</button>
-    </div>
+    <>
+      <Title>Dad Jokes 🤹🏻‍♂️</Title>
+      <div>
+        {joke === "" ? (
+          <button onClick={handleClick}>Get a Joke</button>
+        ) : (
+          <p>Dad Jokes: {joke}</p>
+        )}
+      </div>
+      <button onClick={() => setToggle(!toggle)}>Show me the giggles</button>
+      {toggle && <span>🙈😂🙉</span>}
+      <p>{randomJokeReview}</p>
+      <button
+        onClick={() => setIndex(Math.floor(Math.random() * buttonText.length))}
+      >
+        Joke Review
+      </button>
+
+      <Link to={"/"}>
+        <NextButton />
+      </Link>
+
+      <Link to={"/rate-joke"}>
+        <BackButton />
+      </Link>
+    </>
   );
 };
 
 export default JokeFetch;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  color: #f79d44;
+`;

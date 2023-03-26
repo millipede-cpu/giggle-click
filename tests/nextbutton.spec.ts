278d40562ea8b56283f4ec4232521ea78f340db9
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-03-27 00:18:40 +0100

    installed updates for playwright and experimenting with playwright tests
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- tests/nextbutton.spec.ts
+++ tests/nextbutton.spec.ts
@@ -25,3 +25,31 @@ test("Button component is visible on multiple pages", async () => {
   await page.locator('button[class="next-button"]').click();
   await page.goto("http://localhost:5173");
 });
+
+// import { chromium, test, expect } from "@playwright/test";
+// import describe from "@playwright/test";
+
+// describe("BackButtonWrapper", () => {
+//   test("clicking the button navigates to the specified route", async () => {
+//     // Launch a new Chromium browser instance
+//     const browser = await chromium.launch();
+
+//     // Create a new page in the browser
+//     const page = await browser.newPage();
+
+//     // Navigate to the page with the BackButtonWrapper component
+//     await page.goto("http://localhost:5173/back-button-wrapper");
+
+//     // Click the button
+//     await page.click("button");
+
+//     // Get the URL of the current page
+//     const url = await page.url();
+
+//     // Assert that the URL is equal to the specified route
+//     expect(url).toBe("http://localhost:5173/next-page");
+
+//     // Close the browser
+//     await browser.close();
+//   });
+// });

fbddc494e023c3acb56bc8f3071cf9b72e261a61
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-03-26 23:50:25 +0100

    updated next and back buttons
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- app/src/CardFlipGame.tsx
+++ app/src/CardFlipGame.tsx
@@ -1,23 +1,22 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import BackButton from "./BackButton";
 import NextButton from "./NextButton";
 
-export default function CardFlipGame() {
+interface Props {}
+
+const CardFlipGame: Props = (): JSX.Element => {
   return (
     <>
-      <Link to={"/rate-joke"}>
-        <NextButton />
-      </Link>
-      <Link to={"/joke-randomiser"}>
-        <BackButton />
-      </Link>
+      <NextButton to={"/rate-joke"} />
+      <BackButton to={"/joke-randomiser"} />
       <Title>Card Flip Game 🃟⤵️🃏</Title>
     </>
   );
-}
+};
 
 const Title = styled.h1`
   font-size: 3rem;
   color: #0eb0f3;
 `;
+
+export default CardFlipGame;

7d2af69ffdf1bfca24ceabab20ef2500857a650c
Milly-Palmer <milly@actioncytech.com>
2023-03-26 20:00:33 +0100

    Update main.tsx file with react version 18 imports

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+/** React Version 18 has included updates where ReactDOM
+ *  is not needed to be added before functions such as createRoot,
+ * and this can be imported directly from react-dom/client
+ * More information here 👉🏻 👀 https://react.dev/blog/2022/03/08/react-18-upgrade-guide*/
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = createRoot(rootElement!);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );

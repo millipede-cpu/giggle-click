8696f6965f06d3e60ac42e2f038111d363b6511f
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-03-28 23:24:51 +0100

    error handling on try block of navigation, if it throws error then it'll catch it
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- app/src/NextButton.tsx
+++ app/src/NextButton.tsx
@@ -8,8 +8,18 @@ export default function BackButtonWrapper({ to }: Props): JSX.Element {
   const navigate = useNavigate();
 
   const handleNext = () => {
-    navigate(to);
+    try {
+      navigate(to);
+    } catch (error) {
+      // handle the error here
+      console.error("Error:", error);
+    }
   };
 
   return <button onClick={handleNext}>Next 🤡🤡🤡</button>;
 }
+
+// We added a try block around the call to navigate(to). This is the part of the code that could potentially throw an error.
+// If an error is thrown during the execution of the try block, the code will jump to the catch block.
+// In the catch block, we log the error to the console using console.error.
+// By adding this error handling, we're making our code more robust and defensive. If something unexpected happens during the execution of the component, we'll be able to handle it gracefully and provide feedback to the user.

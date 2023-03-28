42fe34731d81eeb985e49867ebff897716621988
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-03-28 23:05:04 +0100

    error handling with try catch logic for the back button
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- app/src/BackButton.tsx
+++ app/src/BackButton.tsx
@@ -8,7 +8,12 @@ export default function BackButtonWrapper({ to }: Props): JSX.Element {
   const navigate = useNavigate();
 
   const handleBack = () => {
-    navigate(to);
+    try {
+      navigate(to);
+    } catch (error) {
+      console.error("An error occurred while navigating:", error);
+      // handle the error as appropriate, e.g. show an error message to the user
+    }
   };
 
   return <button onClick={handleBack}>Back 🤡🤡🤡</button>;

2241ff0ad4b1203f6dc35fcb8e6b4c5e0355e55b
Milly-Palmer <milly@actioncytech.com>
2023-04-02 01:16:36 +0100

    Wrap ErrorBoundary around CardFlipGame
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- app/src/CardFlipGame.tsx
+++ app/src/CardFlipGame.tsx
@@ -1,37 +1,41 @@
 import styled from "styled-components";
 import BackButton from "./BackButton";
 import NextButton from "./NextButton";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./App";
 
-interface Props {}
-
-const CardFlipGame: Props = (): JSX.Element => {
+export default function CardFlipGame(): JSX.Element {
   try {
     // Your code that could potentially throw an error goes here
+
     return (
-      <>
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={(details) => {
+          // Reset the state of your app so the error doesn't happen again
+        }}
+      >
         <NextButton to={"/rate-joke"} />
         <BackButton to={"/joke-randomiser"} />
         <Title>Card Flip Game 🃟⤵️🃏</Title>
-      </>
+      </ErrorBoundary>
     );
   } catch (error) {
     // Your error handling code goes here
     console.error("An error occurred:", error);
     return (
       <p>
-        😱😱😱😱😱😱😱😱😱😱😱😱 Sorry, something went wrong. Theres no flip
-        game for us today NOOOOOOO!!!!!!! 😲😲😲😲😲😲😲😲
+        😱😱😱😱😱😱😱😱😱😱😱😱 Sorry, something went wrong. Theres no card
+        flip game for us today NOOOOOOO!!!!!!! 😲😲😲😲😲😲😲😲
       </p>
     );
   }
-};
+}
 
 const Title = styled.h1`
   font-size: 3rem;
   color: #0eb0f3;
 `;
-
-export default CardFlipGame;
 
 // wrapped the code that could potentially throw an error in a try block. If an error is thrown, execution will jump to the catch block, where you can handle the error in whatever way you see fit.
 

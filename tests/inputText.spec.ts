bc7ff8885f3e4ce801cdb3ea7faee3d0c5dc4f4a
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-04-11 23:12:45 +0100

    updated method to access input element by placeholder
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- tests/inputText.spec.ts
+++ tests/inputText.spec.ts
@@ -6,6 +6,9 @@ test("Typing a joke and pun should only appear on the page after clicking the Su
   // 1. user enters page.
   await page.goto("http://localhost:5173");
   // 2. user inputs text with placeholder.
+  await page
+    .getByPlaceholder("Type something funny")
+    .fill("I used to be addicted to soap, but I'm clean now.");
   // 3. user clicks addjoke button.
   // 4. joke displayed on page.
   // 5. user inputs text with placeholder whats the pun?

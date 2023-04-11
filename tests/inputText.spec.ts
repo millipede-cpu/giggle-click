61cd634157fadcdfbc2ac03c1568972bc2199653
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-04-11 23:30:53 +0100

    using a browser method to launch chromium to preview the visual steps of the test and locating showjokepun class with filled placeholder text
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- tests/inputText.spec.ts
+++ tests/inputText.spec.ts
@@ -1,8 +1,13 @@
 import { chromium, test } from "@playwright/test";
 
-test("Typing a joke and pun should only appear on the page after clicking the Submit Joke button", async ({
-  page,
-}) => {
+test("Typing a joke and pun should only appear on the page after clicking the Submit Joke button", async () => {
+  // Simualte a chromium browser with an attribute to slow the running time down by 3 seconds so that you can visually see the test going through each step.
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 3000,
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
   // 1. user enters page.
   await page.goto("http://localhost:5173");
   // 2. user inputs text with placeholder.
@@ -12,6 +17,11 @@ test("Typing a joke and pun should only appear on the page after clicking the Su
   // 3. user clicks addjoke button.
   await page.locator(".submit-joke").click();
   // 4. joke displayed on page.
+  await page
+    .locator(
+      '.showJokePun:has-text("I used to be addicted to soap, but I\'m clean now.")'
+    )
+    .isVisible();
   // 5. user inputs text with placeholder whats the pun?
   // 6. user clicks addpun button.
   // 7. pun displayed next to joke on page.

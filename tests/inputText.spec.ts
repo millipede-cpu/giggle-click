60f44dc5fb480b4cd4ce95f5411fddab09c9b3d1
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-04-11 22:58:56 +0100

    removed old test and wrote psudo as steps
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- tests/inputText.spec.ts
+++ tests/inputText.spec.ts
@@ -1,28 +1,11 @@
 import { chromium, test } from "@playwright/test";
 
 test("Typing a joke and pun should only appear on the page after clicking the Submit Joke button", async () => {
-  const browser = await chromium.launch({
-    headless: false,
-    slowMo: 3000,
-  });
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page
-    .locator("#joke", {
-      hasText: "Why do scientists not trust atoms?",
-    })
-    .locator(".submit-joke")
-    .click({ force: true });
-  await page.locator(
-    '.showJokePun:has-text("Why do scientists not trust atoms?")'
-  );
-  await page
-    .locator("#pun", {
-      hasText: "Because they make up everything.",
-    })
-    .locator(".submit-pun")
-    .click({ force: true });
-  await page.locator(
-    '.showJokePun:has-text("Because they make up everything.")'
-  );
+  // 1. user enters page.
+  // 2. user inputs text with placeholder.
+  // 3. user clicks addjoke button.
+  // 4. joke displayed on page.
+  // 5. user inputs text with placeholder whats the pun?
+  // 6. user clicks addpun button.
+  // 7. pun displayed next to joke on page.
 });

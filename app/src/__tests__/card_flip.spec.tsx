2d13e79eaf7497b7274670318247868471bcfb4a
Milly-Palmer <milly@actioncytech.com>
2023-04-22 17:31:13 +0100

    Remove empty export and RenderResult from jest test
    Co-authored-by: Peter <PJSalter@users.noreply.github.com>
    Co-authored-by: Milly <millipede-cpu@users.noreply.github.com>

--- app/src/__tests__/card_flip.spec.tsx
+++ app/src/__tests__/card_flip.spec.tsx
@@ -1,15 +1,10 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Card from "../Card";
 import React from "react";
-import {
-  render,
-  screen,
-  fireEvent,
-  RenderResult,
-} from "@testing-library/react";
-import Card, { CardProps } from "public/src/Card.tsx";
 
 it("flips cards face up when two cards match", () => {
   const cards = ["A", "A", "B", "B"];
-  render(<Card cards={cards} value="A" faceUp={false} onclick={() => {}} />);
+  render(<Card cards={cards} value="A" faceUp={false} onClick={() => {}} />);
   const [cardA, cardB] = screen.getAllByTestId("card") as [
     HTMLElement,
     HTMLElement
@@ -24,5 +19,3 @@ it("flips cards face up when two cards match", () => {
   expect(cardA.getAttribute("data-face-up")).toBe("true");
   expect(cardB.getAttribute("data-face-up")).toBe("true");
 });
-
-export {};

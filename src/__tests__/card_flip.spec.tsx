a6d4eed6557fd98c7289e135472cb78cc1eecfc7
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-09-21 14:20:20 +0100

    jest tests in construction

--- src/__tests__/card_flip.spec.tsx
+++ src/__tests__/card_flip.spec.tsx
@@ -1,21 +1,21 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import Card from "../Card";
-import React from "react";
+// import { render, screen, fireEvent } from "@testing-library/react";
+// import Card from "../Card";
+// import React from "react";
 
-it("flips cards face up when two cards match", () => {
-  const cards = ["A", "A", "B", "B"];
-  render(<Card cards={cards} value="A" faceUp={false} onClick={() => {}} />);
-  const [cardA, cardB] = screen.getAllByTestId("card") as [
-    HTMLElement,
-    HTMLElement
-  ];
+// it("flips cards face up when two cards match", () => {
+//   const cards = ["A", "A", "B", "B"];
+//   render(<Card cards={cards} value="A" faceUp={false} onClick={() => {}} />);
+//   const [cardA, cardB] = screen.getAllByTestId("card") as [
+//     HTMLElement,
+//     HTMLElement
+//   ];
 
-  expect(cardA.getAttribute("data-face-up")).toBe("false");
-  expect(cardB.getAttribute("data-face-up")).toBe("false");
+//   expect(cardA.getAttribute("data-face-up")).toBe("false");
+//   expect(cardB.getAttribute("data-face-up")).toBe("false");
 
-  fireEvent.click(cardA);
-  fireEvent.click(cardB);
+//   fireEvent.click(cardA);
+//   fireEvent.click(cardB);
 
-  expect(cardA.getAttribute("data-face-up")).toBe("true");
-  expect(cardB.getAttribute("data-face-up")).toBe("true");
-});
+//   expect(cardA.getAttribute("data-face-up")).toBe("true");
+//   expect(cardB.getAttribute("data-face-up")).toBe("true");
+// });

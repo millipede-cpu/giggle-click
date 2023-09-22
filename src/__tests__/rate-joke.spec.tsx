9d4c427ea040b299aaffb7b313c71d2589a0d197
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-09-22 14:01:29 +0100

    test being configured

--- src/__tests__/rate-joke.spec.tsx
+++ src/__tests__/rate-joke.spec.tsx
@@ -1,26 +1,26 @@
-// import { render, screen, fireEvent } from "@testing-library/react";
-// import RateJoke from "../RateJoke";
-// import React from "react";
-// import "@testing-library/jest-dom"; // import the jest-dom library
+import { render, screen, fireEvent } from "@testing-library/react";
+import RateJoke from "../RateJoke";
+import React from "react";
+import "@testing-library/jest-dom"; // import the jest-dom library
 
-// describe("RateJoke", () => {
-//   it("renders the happy and sad buttons", () => {
-//     render(<RateJoke />);
-//     expect(screen.getByText("Happy")).toBe(true);
-//     expect(screen.getByText("Sad")).toBe(true);
-//   });
+describe("RateJoke", () => {
+  it("renders the happy and sad buttons", () => {
+    render(<RateJoke />);
+    expect(screen.getByText("Happy")).toBe(true);
+    expect(screen.getByText("Sad")).toBe(true);
+  });
 
-//   it("updates the happy state when the happy button is clicked", () => {
-//     render(<RateJoke />);
-//     const happyButton = screen.getByText("Happy");
-//     fireEvent.click(happyButton);
-//     expect(screen.getByText("😊")).toBe(true);
-//   });
+  it("updates the happy state when the happy button is clicked", () => {
+    render(<RateJoke />);
+    const happyButton = screen.getByText("Happy");
+    fireEvent.click(happyButton);
+    expect(screen.getByText("😊")).toBe(true);
+  });
 
-//   it("updates the sad state when the sad button is clicked", () => {
-//     render(<RateJoke />);
-//     const sadButton = screen.getByText("Sad");
-//     fireEvent.click(sadButton);
-//     expect(screen.getByText("😥")).toBe(true);
-//   });
-// });
+  it("updates the sad state when the sad button is clicked", () => {
+    render(<RateJoke />);
+    const sadButton = screen.getByText("Sad");
+    fireEvent.click(sadButton);
+    expect(screen.getByText("😥")).toBe(true);
+  });
+});

7d4ee859fa1ad9c4715b4ff61e541978942955fc
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-09-22 17:47:20 +0100

    passed tests for if happy and sad buttons are clicked 10 times and updates this count for the state

--- src/__tests__/rate-joke.spec.tsx
+++ src/__tests__/rate-joke.spec.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import RateJoke from "../RateJoke";
 import TestErrorBoundary from "../TestErrorBoundary";
+import CustomErrorBoundaryWrapper from "../CustomErrorBoundaryWrapper";
 
 describe("RateJoke", () => {
   test("renders the happy and sad buttons", () => {
@@ -83,31 +84,57 @@ describe("RateJoke", () => {
     expect(sadEmoji).toBeNull();
   });
 
-  test("clicking the Sad button 10 times updates the sad state", () => {
-    render(<RateJoke />);
-
-    const sadButton = screen.getByText("Sad");
-
-    // Simulate clicking the Sad button 10 times
-    for (let i = 0; i < 10; i++) {
-      fireEvent.click(sadButton);
-    }
-
-    const sadEmoji = screen.getByTestId("sad-emoji");
-    expect(sadEmoji).toBeTruthy(); // Ensure that the sad emoji is displayed
-  });
-
   test("clicking the Happy button 10 times updates the happy state", () => {
-    render(<RateJoke />);
+    render(
+      <CustomErrorBoundaryWrapper>
+        <MemoryRouter>
+          <RateJoke />
+        </MemoryRouter>
+      </CustomErrorBoundaryWrapper>
+    );
 
-    const happyButton = screen.getByText("Happy");
-
-    // Simulate clicking the Happy button 10 times
+    const happyButton = screen.getByTestId("happy-button"); // Select the Happy button by data-testid
     for (let i = 0; i < 10; i++) {
       fireEvent.click(happyButton);
     }
 
-    const happyEmoji = screen.getByTestId("happy-emoji");
-    expect(happyEmoji).toBeTruthy(); // Ensure that the happy emoji is displayed
+    // Define a custom text matcher function to find the emoji
+    const findHappyEmoji = (content: string, element: Element | null) => {
+      return (
+        element &&
+        element.tagName.toLowerCase() === "span" &&
+        element.classList.contains("happy") &&
+        /😄/.test(content)
+      );
+    };
+
+    expect(findHappyEmoji).toBeTruthy();
+  });
+
+  test("clicking the Sad button 10 times updates the sad state", () => {
+    render(
+      <CustomErrorBoundaryWrapper>
+        <MemoryRouter>
+          <RateJoke />
+        </MemoryRouter>
+      </CustomErrorBoundaryWrapper>
+    );
+
+    const sadButton = screen.getByTestId("sad-button"); // Select the Sad button by data-testid
+    for (let i = 0; i < 10; i++) {
+      fireEvent.click(sadButton);
+    }
+
+    // Define a custom text matcher function to find the emoji
+    const findSadEmoji = (content: string, element: Element | null) => {
+      return (
+        element &&
+        element.tagName.toLowerCase() === "span" &&
+        element.classList.contains("sad") &&
+        /😢/.test(content)
+      );
+    };
+
+    expect(findSadEmoji).toBeTruthy();
   });
 });

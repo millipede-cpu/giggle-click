a089ff485186b66b6b7bf41c5064fe867ec7f3f1
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-09-22 16:29:28 +0100

    data-testid for happy and sad to that jest tests will pick up these elements in the component

--- src/RateJoke.tsx
+++ src/RateJoke.tsx
@@ -17,12 +17,20 @@ export default function RateJoke() {
         <button className="happy-click" onClick={() => setHappy(!happy)}>
           Happy
         </button>
-        {happy ? <span className="happy">&#128514;</span> : null}
+        {happy ? (
+          <span className="happy" data-testid="happy-emoji">
+            &#128514;
+          </span>
+        ) : null}
 
         <button className="sad-click" onClick={() => setSad(!sad)}>
           Sad
         </button>
-        {sad ? <span className="sad">&#128529;</span> : null}
+        {sad ? (
+          <span className="sad" data-testid="sad-emoji">
+            &#128529;
+          </span>
+        ) : null}
       </>
     );
   } catch (error) {

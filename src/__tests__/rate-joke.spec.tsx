a93dc21e84e3607c8851758b6f637839a3f7a580
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-09-22 17:48:52 +0100

    removed waitFor from import path

--- src/__tests__/rate-joke.spec.tsx
+++ src/__tests__/rate-joke.spec.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, screen, waitFor } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import RateJoke from "../RateJoke";
 import TestErrorBoundary from "../TestErrorBoundary";

ae61c85c2a2f487ff1a8a7cf359becebf8055a70
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-09-22 16:39:53 +0100

    imported custom error boundary wrapper for error handling on card flip test

--- src/__tests__/card_flip.spec.tsx
+++ src/__tests__/card_flip.spec.tsx
@@ -1,15 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Card from "../Card"; // Import your Card component
 import React from "react";
+import CustomErrorBoundaryWrapper from "../CustomErrorBoundaryWrapper";
 
 it("renders a card with data-face-up true", () => {
   render(
-    <Card
-      value="A"
-      faceUp={true}
-      onClick={() => {}}
-      cards={[]} // Provide a dummy cards prop
-    />
+    <CustomErrorBoundaryWrapper>
+      <Card
+        value="A"
+        faceUp={true}
+        onClick={() => {}}
+        cards={[]} // Provide a dummy cards prop
+      />
+    </CustomErrorBoundaryWrapper>
   );
   const card = screen.getByTestId("card") as HTMLElement;
 

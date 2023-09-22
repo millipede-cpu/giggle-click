9d86869a39c8cd6b85b9375e6b6452f6a96c34ff
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-09-22 14:00:58 +0100

    removed commented code that was not needed

--- src/RateJoke.tsx
+++ src/RateJoke.tsx
@@ -3,28 +3,6 @@ import BackButton from "./BackButton";
 import NextButton from "./NextButton";
 import React from "react";
 
-// export default function RateJoke() {
-//   const [happy, setHappy] = useState(false);
-//   const [sad, setSad] = useState(false);
-
-//   return (
-//     <>
-//       <NextButton to={"/joke-fetch"} />
-//       <BackButton to={"/card-flip-game"} />
-//       <h1>Rate Joke 👍🏼🎭👎🏼</h1>
-//       <button className="happy-click" onClick={() => setHappy(!happy)}>
-//         Happy
-//       </button>
-//       {happy ? <span className="happy">&#128514;</span> : null}
-
-//       <button className="sad-click" onClick={() => setSad(!sad)}>
-//         Sad
-//       </button>
-//       {sad ? <span className="sad">&#128529;</span> : null}
-//     </>
-//   );
-// }
-
 export default function RateJoke() {
   const [happy, setHappy] = useState(false);
   const [sad, setSad] = useState(false);
